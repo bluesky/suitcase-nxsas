bb7deb7dceb367242b82d1761af7e96a93d8a7fd
Joshua Lynch <jlynch@bnl.gov>
2020-06-25 15:52:46 -0400

    comment unused imports for now

--- suitcase/nxsas/tests/conftest.py
+++ suitcase/nxsas/tests/conftest.py
@@ -5,13 +5,13 @@ import h5py
 import pytest
 from bluesky.tests.conftest import RE  # noqa
 from ophyd.tests.conftest import hw  # noqa
-from suitcase.utils.tests.conftest import (  # noqa
-    example_data,
-    generate_data,
-    plan_type,
-    detector_list,
-    event_type,
-)  # noqa
+# from suitcase.utils.tests.conftest import (  # noqa
+#     example_data,
+#     generate_data,
+#     plan_type,
+#     detector_list,
+#     event_type,
+# )  # noqa
 
 
 @pytest.fixture
